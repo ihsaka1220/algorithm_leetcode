e3eda568fa81cd9c006b728f2a99b3a1c6d8ca81
sekai <1913519407@qq.com>
2026-01-15 14:25:25 +0800

    51-hard-n皇后 n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。 给你一个整数 n ，返回所有不同的 n 皇后问题 的解决方案。 每一种解法包含一个不同的 n 皇后问题 的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。

---
+++ src/solveNQueens.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class solveNQueens {
+
+    List<List<String>> results = new ArrayList<>();
+    public List<List<String>> solveNQueens(int n) {
+        char[][] chessBoard = new char[n][n];
+        for(char[] c:chessBoard){
+            Arrays.fill(c,'.');
+        }
+        backTrack(chessBoard,0,n);
+        return results;
+    }
+
+    void backTrack(char[][] chessBoard, int row, int n) {
+        if(row==n){
+            results.add(Array2List(chessBoard));
+            return;
+        }
+        for(int col = 0; col<n; col++){
+            //左上角、右上角不是
+            if(isVaild(chessBoard,row,col,n)){
+                chessBoard[row][col]='Q';
+                backTrack(chessBoard,row+1,n);
+                chessBoard[row][col]='.';
+            }
+        }
+    }
+
+    private boolean isVaild(char[][] chessBoard, int row, int col, int n) {
+        if(row==0&&col==0){
+            return true;
+        }
+        for(int x=row-1,y=col-1;x>=0 && y>=0;x--,y--){
+            if(chessBoard[x][y]=='Q'){
+                return false;
+            }
+        }
+        for(int x=row-1,y=col+1;x>=0 && y<n;x--,y++){
+            if(chessBoard[x][y]=='Q'){
+                return false;
+            }
+        }
+        for(int y = row-1; y >=0; y--){
+            if(chessBoard[y][col]=='Q'){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private List<String> Array2List(char[][] chessBoard) {
+        List<String> res=new ArrayList<>();
+        for(char[] c: chessBoard){
+            res.add(new String(c));
+        }
+        return res;
+    }
+}
