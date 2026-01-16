a659f123433062fc6bde18cc3cfec8f072f33510
sekai <1913519407@qq.com>
2026-01-16 17:22:01 +0800

    feat:455-easy 分发饼干 feat:376-medium 摆动序列

---
+++ src/greedy/wiggleMaxLength.java
@@ -0,0 +1,20 @@
+package greedy;
+
+public class wiggleMaxLength {
+    public int wiggleMaxLength(int[] nums) {
+        if(nums.length==1){
+            return 1;
+        }
+        int curDiff=0;
+        int preDiff=0;
+        int count=1;
+        for(int i=1;i<nums.length;i++){
+            curDiff=nums[i]-nums[i-1];
+            if(curDiff>0 && preDiff<=0 || curDiff<0 && preDiff>=0){
+                count++;
+                preDiff=curDiff;
+            }
+        }
+        return count;
+    }
+}
